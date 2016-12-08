22b490805aeb03ed4dd4121a3b337adf68a980f6
Dylan Metas <dylanmetas@gmail.com>
2016-12-07 21:47:18 -0800

    added display message

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,26 +1,36 @@
 puts"Enter Hamster name."
 hamsterName = gets.chomp
+
 puts"Hamster volume level from 1-10"
 volume = gets.chomp.to_i
+
 puts"what is the fur color?"
 furColor = gets.chomp
+
+# Loop until user gives valid input
 candidate = nil
 until candidate == "y"||candidate == "Y"||candidate == "n"|| candidate == "N" || candidate == true || candidate == false
-	puts"Hamster is a good candidate for adoption Y or N"
-	candidate = gets.chomp
-	if candidate == "y"||candidate == "Y"
-		candidate = true
-	elsif candidate == "n"||candidate == "N"
-		candidate = false
-	else
-		puts"please enter Y or N"
-	end
-end	
+  puts"Hamster is a good candidate for adoption Y or N"
+  candidate = gets.chomp
+  if candidate == "y"||candidate == "Y"
+    candidate = true
+  elsif candidate == "n"||candidate == "N"
+    candidate = false
+  else
+    puts"please enter Y or N"
+  end
+end  
 puts"estimated age"
 age = gets.chomp
 if age == ""
-	age = nil
+  age = nil
 else
-	age = age.to_f
+  age = age.to_f
 end
-print(hamsterName, volume, furColor, candidate,age)
+
+puts"Your Hamster information:"
+puts"Your Hamsters name: #{hamsterName}"
+puts"Your Hamsters volume: #{volume}"
+puts"Your Hamsters fur color: #{furColor}"
+puts"Your hamasters a good candidate: #{candidate}"
+puts"Your Hamsters age is: #{age}"
