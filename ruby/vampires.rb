cc32798cddfd90d3ced1a7b983be6dafb45b9671
Dylan Metas <dylanmetas@gmail.com>
2016-12-08 11:49:37 -0800

    created form template

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "what is your name?"
+userName = gets.chomp
+puts "How old are you? what year were you born in?"
+age = gets.chomp
+year = gets.chomp
+puts "Would you like us to order garlic bread for you? (please answer Yes or No)"
+bread = gets.chomp
+puts "would you like to enroll in our healthcare plan? (please answer Yes or No"
+healthcare = gets.chomp
+
