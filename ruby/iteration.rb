842a2bb1e372dfda7909f8c374675d6e8e5182c0
Dylan Metas <dylanmetas@gmail.com>
2016-12-13 18:27:23 -0800

    created arrays hash and moddified array with .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -3,3 +3,41 @@ def say_goodbye
 	yield("Bob", "Tinna")
 end
 say_goodbye {|name1,name2| puts "Terrible to see you #{name1} and #{name2}"}
+
+
+customer_names = ["Bob", "James", "Marry", "Eletabeth", "Dog"]
+puts "origonal data:"
+p customer_names
+
+customer_names.each do |x| 
+	puts "the customer name is: #{x}"
+end
+
+
+customer_age do [24,33,21,67,89]
+puts "origional data:"
+p customer_age
+
+customer_age = [24,33,21,67,89]
+puts "origional data:"
+p customer_age
+customer_age.map! do |age| 
+	puts age
+	age+=1
+end
+puts "after .map call:"
+p customer_age
+
+
+fruit_color = {
+	:apple => "red",
+	:Banana => "yellow",
+	:orange => "orange",
+	:grape => "purple"
+}
+
+fruit_color.each do |fruit, color|
+	puts "the #{fruit} is #{color}"
+end
+
+
