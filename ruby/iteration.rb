e461b778624d37c4ad31e0acaf0c7cd859ff0b47
Dylan Metas <dylanmetas@gmail.com>
2016-12-13 19:11:29 -0800

    change hash and array data

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -40,4 +40,43 @@ fruit_color.each do |fruit, color|
 	puts "the #{fruit} is #{color}"
 end
 
+#iteration number 1
+numbers = [1,2,3,3,3,4,5,]
+numbers.delete 3
+
+fruits_price2 = {
+  :apple => 3,
+  :banana => 2,
+  :pear => 3
+}
+fruits_price2.delete_if{|k,v| v == 3}
+
+#iteration number 2
+number_list2 = [1,2,3,4,5,6,7,8,9,10]
+number_list2.select{|number| number.even?}
+
+fruits_price = {
+  :apple => 3,
+  :banana => 2,
+  :pear => 3
+}
+fruits_price.select {|k,v| v == 3}
+
+#iteration number 3
+names = ["bob", "bob", "bob", "james"]
+names.uniq!
+
+fruits_price3 = {
+  :apple => 3,
+  :banana => 2,
+  :pear => 3,
+  :peach => 2
+}
+fruits_price3.reject!{|k,v| v==2}
+
+#iteration number 4
+
+
+
+
 
