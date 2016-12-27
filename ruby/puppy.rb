e28b5198a19f6e1d03dce984028277a08dd98afc
Dylan Metas <dylanmetas@gmail.com>
2016-12-26 21:22:17 -0800

    completed 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+class Puppy
+end
+=begin
+#species of dog
+SPECIES
+hound
+#characteristics of hounds
+CHARACTERISTICS
+ears: long
+snout: medium
+eye count: 2
+friendly: yes
+Name: varies
+Gender: varies
+Color: varies
+#actions dogs take. 
+BEHAVIOR
+lick
+bark
+rollover
+play dead
+=end
