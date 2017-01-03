7654374afc3102e8baa34676ef3b64739b4de7f9
Dylan Metas <dylanmetas@gmail.com>
2017-01-02 17:00:24 -0800

    add mixin module for shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
 	def self.yell_angrily(words)
@@ -11,3 +12,22 @@ end
 
 puts Shout.yell_happily( "I'm so happy")
 puts Shout.yell_angrily("I'm so angry")
+=end
+module Shout
+	def angry_shout(text)
+		text + "!!!"
+	end
+end
+
+class Person
+	include Shout
+end
+
+class Monkey
+	include Shout
+end
+
+james = Person.new
+puts james.angry_shout("SO ANGRY")
+monkey = Monkey.new
+puts monkey.angry_shout("NOISE")
