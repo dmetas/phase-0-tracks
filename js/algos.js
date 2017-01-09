43231477d420fd4a0a869b430b64176f248b7b54
Dylan Metas <dylanmetas@gmail.com>
2017-01-08 22:40:36 -0800

    created test generator data and passed to longest word method

--- js/algos.js
+++ js/algos.js
@@ -23,14 +23,31 @@ function value_check (obj1, obj2){
 	return false;
 }
 
+function test_generator(number){
+	var words = [];
+	var alp = "abcdefghijklmnopqrstuvwxyz".split("");
+	while(words.length < number) {
+		var length = Math.floor(Math.random() * 10) + 1;
+		var new_word = "";
+		for (var i = 0; i <= length; i++) {
+			var new_letter = Math.floor(Math.random() * 25) + 0;
+			new_word += alp[new_letter];
+		}
+		words.push(new_word);
+	}
+	return words;
+}
+
 
 //driver code
 var names = ["David", "Kat", "Bob"];
 console.log(longest_string(names));
-
 var obj1 = {name: "Steven", age: 54};
 var obj2 = {name: "Tamir", age: 54};
 console.log(value_check(obj1, obj2));
 var steven = {name: "Steven", age: 54};
 var tamir = {name: "Tamir", age: 5};
 console.log(value_check(steven, tamir));
+var test_data = test_generator(10);
+console.log(test_data);
+console.log(longest_string(test_data));
